823a92e31a3f40b9513e4576bf0a489a5d20362e
jhorman <jhorman@gmail.com>
2015-04-14 15:09:09 -0700

    Removing some unused stuff

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -24,26 +24,3 @@ def touch(path):
 
 def post_fork(server, worker):
     touch('/tmp/app-initialized')
-
-
-def post_request(worker, req, environ, resp):
-    from server.dao import db
-    import psycopg2.extensions as _ext
-
-    if not db.is_closed():
-        status = db.get_conn().get_transaction_status()
-        if status != _ext.TRANSACTION_STATUS_IDLE:
-            db.rollback()
-        db.close()
-
-    for slave in db.slaves:
-        if not slave.is_closed():
-            status = slave.get_conn().get_transaction_status()
-            if status != _ext.TRANSACTION_STATUS_IDLE:
-                slave.rollback()
-            slave.close()
-
-
-def worker_exit(server, worker):
-    from blade.services.bus import shutdown
-    shutdown.send()
