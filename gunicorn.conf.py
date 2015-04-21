ef24e5f5f89a64ea2e855cabfa52e18a0efd9259
jhorman <jhorman@gmail.com>
2015-04-21 11:37:29 -0700

    Removing extra buildpacks, for now at least

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,6 +1,6 @@
 import os
 
-bind = 'unix:/tmp/nginx.socket'
+bind = '0.0.0.0:%s' % os.environ.get('PORT', 5200)
 timeout = 15
 graceful_timeout = 10
 backlog = 100
@@ -8,19 +8,3 @@ max_requests = 0
 workers = int(os.environ.get('WEB_NUM_PROCS', 10))
 loglevel = 'warn'
 preload_app = False  # for safety ALWAYS leave this FALSE, esp if gevent is on
-
-
-def touch(path):
-    import os, time
-
-    now = time.time()
-    try:
-        # assume it's there
-        os.utime(path, (now, now))
-    except os.error:
-        open(path, "w").close()
-        os.utime(path, (now, now))
-
-
-def post_fork(server, worker):
-    touch('/tmp/app-initialized')
