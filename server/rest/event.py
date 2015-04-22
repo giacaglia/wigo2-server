68d037e33aca69fbe5518e437e6258d3331498f9
jhorman <jhorman@gmail.com>
2015-04-22 13:08:50 -0700

    Adding "voted" to message metadata

--- server/rest/event.py
+++ server/rest/event.py
@@ -224,7 +224,8 @@ class UserEventMessagesMetaListResource(WigoResource):
         message_ids = wigo_db.sorted_set_range(user_eventmessages_key(g.user, event_id), 0, -1)
         for message_id in message_ids:
             message_meta[message_id] = {
-                'num_votes': wigo_db.get_sorted_set_size(skey('message', message_id, 'votes'))
+                'num_votes': wigo_db.get_sorted_set_size(skey('message', message_id, 'votes')),
+                'voted': wigo_db.sorted_set_is_member(skey('message', message_id, 'votes'), g.user.id)
             }
 
         return message_meta
