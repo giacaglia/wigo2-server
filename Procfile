59c38600e6788473cfee2a05cf0056fdfc4394b7
jhorman <jhorman@gmail.com>
2015-06-01 15:04:05 -0700

    Turn nginx back on

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
-web: newrelic-admin run-program gunicorn -c gunicorn.conf.py web:app
+web: bin/start-nginx newrelic-admin run-program gunicorn -c gunicorn.conf.py web:app
 worker: newrelic-admin run-program python start_worker.py -c worker
 sync: python rdbms_sync.py
--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,6 +1,6 @@
 import os
 
-bind = '0.0.0.0:%s' % os.environ.get('PORT', 5200)
+bind = 'unix:/tmp/nginx.socket'
 timeout = 15
 graceful_timeout = 10
 backlog = 100
@@ -10,3 +10,19 @@ worker_class = 'gevent'
 worker_connections = int(os.environ.get('WEB_GEVENT_POOL_SIZE', 50))
 loglevel = 'warn'
 preload_app = False  # for safety ALWAYS leave this FALSE, esp if gevent is on
+
+
+def touch(path):
+    import os, time
+
+    now = time.time()
+    try:
+        # assume it's there
+        os.utime(path, (now, now))
+    except os.error:
+        open(path, "w").close()
+        os.utime(path, (now, now))
+
+
+def post_fork(server, worker):
+    touch('/tmp/app-initialized')
